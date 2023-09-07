10f2acf6c5a0cbe5c2a0c2acc6857390d2b50c8c
Egnom.AI <114785955+Egnom-Ai@users.noreply.github.com>
2023-09-07 16:54:37 -0600

    first commit

---
+++ SD_OMF_4E/SD_OMF_4EApp.swift
@@ -0,0 +1,17 @@
+//
+//  SD_OMF_4EApp.swift
+//  SD_OMF_4E
+//
+//  Created by Gustavo Monge on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct SD_OMF_4EApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
